4b992972336141ac888e2c2e8fd4ab341adda2e3
leoc <leoc@aspectmicrosystems.com.tw>
2014-09-19 11:55:07 +0800

    [MS] initial file

---
+++ source/mothership.c
@@ -0,0 +1,18 @@
+
+
+
+//memory alloc. put in/put out
+
+//debug printf
+
+//time measurement, start /stop
+
+//file rw open, save to file for debug
+
+//main() 
+//p0: control, monitor, and debug
+//p1: wifi socket connection
+//p2: spi0 
+//p3: spi1
+
+
