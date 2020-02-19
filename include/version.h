79d8b8276a8db4c9d4cbd8f6d8e6c39fb7b9df05
Leo Chen <leoc@aspectmicrosystems.com.tw>
2020-02-19 15:16:07 +0800

    [RELEASE] fix first meta hanging issue

--- include/version.h
+++ include/version.h
@@ -1,1 +1,1 @@
-MSP Version v0.1.9
+MSP Version v0.1.10
--- source/mothership.c
+++ source/mothership.c
@@ -53,8 +53,8 @@ int pipe2(int pipefd[2], int flags);
 //main()
 // version example: MSP Version v0.0.2, 2019-03-13 13:36:30 f2be242, 2019.12.17 14:48:18
 
-static char mver[] = "MSP Version v0.1.10.rc";
-static char gitcommit[] = "2019-12-03 14:08:51 35139c7";
+static char mver[] = "MSP Version v0.1.11.rc";
+static char gitcommit[] = "2020-02-18 22:30:49 86801ac";
 static char buildtime[] = __TIMESTAMP__; // 24 
 static char genssid[128];
 
